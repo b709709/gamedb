ce32bed8178898cbe0a311aa7c05596c286d9096
brent butler <b709709@gmail.com>
2026-08-30 17:01:22 -0400

    add the imporst to the python file

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,17 +7,25 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")  # your login page
 
+@app.route("/dashboard")
+def dashboard():
+    return render_template("maindashboard.html")
+
 @app.route("/login", methods=["POST"])
 def login():
     username = request.form.get("username")
     password = request.form.get("password")
 
+    print("username posted:",username)
+    print("password posted:",password)
+
     # Example: return JSON
-    return jsonify({
-        "status": "ok",
-        "username": username,
-        "message": "Login received"
-    })
+    #return jsonify({
+    #    "status": "ok",
+    #    "username": username,
+    #    "message": "Login received"
+    #})
+    return redirect(url_for("dashboard"))
 
 if __name__ == "__main__":
     app.run(debug=True)
