fef7995eebf701845ed54c47db008ffd65929f5d
brent butler <b709709@gmail.com>
2026-08-30 18:31:58 -0400

    login process session data flash

--- server.py
+++ server.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template, request, jsonify
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, flash, session
+from services.auth import process_login
 
 app = Flask(__name__)
+app.secret_key = "709709ab"
 
+@app.before_request
+def require_login():
+    allowed_routes = ["index","login"]
+    if "logged_in" not in session:
+        if request.endpoint not in allowed_routes:
+            return redirect(url_for("index"))
+        
 @app.route("/")
 def index():
     return render_template("index.html")  # your login page
 
 @app.route("/dashboard")
 def dashboard():
+    if not session.get("logged_in"):
+        flash("You must be logged in first","error")
+        return redirect(url_for("index"))
+
+    flash("Welcome:" + session.get("username"),"success")
     return render_template("maindashboard.html")
 
 @app.route("/login", methods=["POST"])
@@ -16,10 +30,17 @@ def login():
     username = request.form.get("username")
     password = request.form.get("password")
 
-    print("username posted:",username)
-    print("password posted:",password)
-
-    # Example: return JSON
+    ok = process_login(username,password)
+    if ok:
+        session["logged_in"] = True
+        session["username"] = username
+        return redirect(url_for("dashboard"))
+    else:
+        flash("login failed. Please try again.","error")
+        flash("unknown user","error")
+        return redirect(url_for("index"))
+    
+        # Example: return JSON
     #return jsonify({
     #    "status": "ok",
     #    "username": username,
---
+++ services/auth.py
@@ -0,0 +1,6 @@
+def process_login(username,password):
+    print("processing login in external py file")
+    print("username",username)
+    print("password",password)
+
+    return True
