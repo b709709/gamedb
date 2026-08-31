a7466bc8834bb4b4632887d507af142313ba7a8c
brent butler <b709709@gmail.com>
2026-08-31 13:39:40 -0400

    got login verify, logout, dashboard with the games list for user

--- server.py
+++ server.py
@@ -1,39 +1,60 @@
 from flask import Flask, render_template, request, jsonify
 from flask import Flask, redirect, url_for, flash, session
 from services.auth import process_login
-from services.db import get_all_games
+from services.db import get_all_games, get_user
 
 app = Flask(__name__)
 app.secret_key = "709709ab"
 
 @app.before_request
 def require_login():
-    allowed_routes = ["index","login"]
+    print("Client Request endpoint",request.endpoint)
+    if request.endpoint and request.endpoint.startswith("static"):
+        return
+    
+    allowed_routes = ["index","login", "static"]
     if "logged_in" not in session:
         if request.endpoint not in allowed_routes:
             return redirect(url_for("index"))
-        
+
+
 @app.route("/")
 def index():
     return render_template("index.html")  # your login page
-
+    
 @app.route("/dashboard")
 def dashboard():
 
+    if 'username' not in session:
+        return redirect('/login')
+    
     if not session.get("logged_in"):
         flash("You must be logged in first","error")
         return redirect(url_for("index"))
 
-    games = get_all_games()
+    print ("get games for",session.get("username"))
+
+    games = get_all_games(session)
+    print("row count",len(games))
+
     flash("Welcome:" + session.get("username"),"success")
     return render_template("maindashboard.html",games=games)
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/?loggedout=1") #go back to the login screen do not use login that process the login
+    
 
 @app.route("/login", methods=["POST"])
 def login():
     username = request.form.get("username")
     password = request.form.get("password")
 
-    ok = process_login(username,password)
+    #ok = process_login(username,password)
+    ok = get_user(username,password)
+    
+
     if ok:
         session["logged_in"] = True
         session["username"] = username
--- services/db.py
+++ services/db.py
@@ -1,4 +1,5 @@
 import psycopg2.extras
+from flask import Flask, redirect, url_for, flash, session
 
 def get_connection():
     return psycopg2.connect(
@@ -9,10 +10,31 @@ def get_connection():
         port="5432"
     )
 
-def get_all_games():
+def get_user(username,password):
+    print(username)
+    print(password)
     conn = get_connection()
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cur.execute("SELECT name,console FROM game;")
+    sql = cur.mogrify("select all from users where username = %s and password = %s",(username,password))
+    print(sql)
+    cur.execute(sql)
+    
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    if len(rows) > 0:
+       return True
+    else:
+       print("No user record found.")
+       return False
+
+def get_all_games(session):
+    conn = get_connection()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    username = session.get('username')
+    print("THE USERNAME IN GET_ALL_GAMES IS",username)
+    sql = cur.mogrify("SELECT name,console from game where username = %s",(username,))
+    cur.execute(sql)
     rows = cur.fetchall()
     cur.close()
     conn.close()
